901a321dfff8e47ec71997fd06d0da0455d23ed1
jsamp90 <jeremysampson1@gmail.com>
2017-10-30 16:00:35 +0000

    more code
    greet capital letters

--- backend/__init__.py
+++ backend/__init__.py
@@ -3,6 +3,16 @@
 def process_user_query(query_string):
     names = query_string.split(' ')
     greetings = [ ]
+
     for name in query_string.split():
         greetings.append(f'Hi {names}!')
     return grettings
+
+
+def greet_upper(query_string):
+    capital = query_string
+    greet_capital = [ ]
+    for char in capital:
+        if char in 'QWERTYUIOPASDFGHJKLZXCVBNM':
+            greet_capital.append(f'Hi {captial}!')
+    return greet_capital
