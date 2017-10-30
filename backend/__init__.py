f65522836f07e40f64f24674ecdf8988057349db
jsamp90 <jeremysampson1@gmail.com>
2017-10-30 14:36:29 +0000

    new code

--- backend/__init__.py
+++ backend/__init__.py
@@ -4,5 +4,5 @@ def process_user_query(query_string):
     names = query_string.split(' ')
     greetings = [ ]
     for name in query_string.split():
-        greetings.append(f'Hi {names}!'')
-    return greet
+        greetings.append(f'Hi {names}!')
+    return grettings
