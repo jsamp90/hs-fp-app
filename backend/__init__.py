066e6e114a80edb25f55ac53c53b15eaa9b30620
jsamp90 <jeremysampson1@gmail.com>
2017-11-01 16:43:14 +0000

    weight loss code
    testing front end

--- backend/__init__.py
+++ backend/__init__.py
@@ -10,9 +10,10 @@ def process_user_query(query_string):
 
 
 def greet_upper(query_string):
-    capital = query_string.split(' ')
+    capital = ['Jeremy', 'Sampson']
     greet_capital = [ ]
     for char in capital[0]:
         if char in 'QWERTYUIOPASDFGHJKLZXCVBNM':
             greet_capital.append(f'Hi {captial}!')
     return greet_capital
+print(greet_capital)
